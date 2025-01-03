91cd6d3338cc7be8a2164e5095773bd90397755a
haanh17 <haanhh.h178@gmail.com>
2025-01-03 08:08:48 +0700

    introlan2

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -10,87 +11,32 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtName, edtPhone, edtEmail, edtMKhau;
-    CheckBox cbCheck;
-    Button btnUp, btnIn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro1);
-
-        AnhXa();
-
-        // Lắng nghe trạng thái checkbox
-        cbCheck.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            // Bật hoặc tắt nút Sign Up dựa trên trạng thái checkbox
-            btnUp.setEnabled(isChecked);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-
-        btnUp.setOnClickListener(new View.OnClickListener() {
+        // Chuyển sang Intro2 sau 2 giây
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                String hoten = edtName.getText().toString();
-                String email = edtEmail.getText().toString();
-                String soDT = edtPhone.getText().toString();
-                String pass = edtMKhau.getText().toString();
-
-                if (!isValidEmail(email) || !isValidPhoneNumber(soDT)) {
-                    Toast.makeText(MainActivity.this, "Email hoặc số điện thoại không đúng định dạng", Toast.LENGTH_SHORT).show();
-                }
-                if (!isValidPassword(pass)) {
-                    Toast.makeText(MainActivity.this, "Mật khẩu ít nhất 8 ký tự gồm chữ hoa, chữ thường, số và ký tự đặc biệt", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        btnIn.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Sign_In.class);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Intro2.class);
                 startActivity(intent);
             }
-        }));
-
-    }
-
-    private void AnhXa() {
-        edtEmail = (EditText) findViewById(R.id.edtEmail);
-        edtName = (EditText) findViewById(R.id.edtTen);
-        edtPhone = (EditText) findViewById(R.id.edtSdt);
-        edtMKhau = (EditText) findViewById(R.id.edtMKhau);
-        cbCheck = findViewById(R.id.cbCheck);
-        btnUp = findViewById(R.id.btnUp);
-        btnIn = findViewById(R.id.btnIn);
-        // Vô hiệu hóa button ban đầu
-        btnUp.setEnabled(false);
-    }
-
-    private boolean isValidEmail(String email) {
-        String emailPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
-        Pattern pattern = Pattern.compile(emailPattern);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-
-    private boolean isValidPhoneNumber(String phoneNumber) {
-        String phonePattern = "^[0-9]{10,11}$";
-        Pattern pattern = Pattern.compile(phonePattern);
-        Matcher matcher = pattern.matcher(phoneNumber);
-        return matcher.matches();
-    }
-
-    private boolean isValidPassword(String password) {
-        String passwordPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!_&*]).{8,}$";
-        Pattern pattern = Pattern.compile(passwordPattern);
-        Matcher matcher = pattern.matcher(password);
-        return matcher.matches();
+        }, 3000);
     }
 }
