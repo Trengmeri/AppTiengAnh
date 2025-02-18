504ef70687b4856ef01d898db31e4bb38b04c100
haanh17 <haanhh.h178@gmail.com>
2025-02-18 14:02:13 +0700

    ui lan n

--- app/src/main/java/com/example/test/adapter/FlashcardAdapter.java
+++ app/src/main/java/com/example/test/adapter/FlashcardAdapter.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.test.R;
 import com.example.test.model.Flashcard;
 import com.example.test.ui.FlashcardInformationActivity;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.List;
 
@@ -45,6 +49,9 @@ public class FlashcardAdapter extends RecyclerView.Adapter<FlashcardAdapter.Flas
             intent.putExtra("FLASHCARD_ID", flashcard.getId()); // Gửi ID flashcard đến FlashcardInformationActivity
             context.startActivity(intent); // Khởi động activity thông tin flashcard
         });
+        // Sự kiện click vào iconRemove để hiển thị dialog xác nhận
+        holder.iconRemove.setOnClickListener(v -> showRemoveDialog(position));
+
     }
 
     @Override
@@ -55,11 +62,34 @@ public class FlashcardAdapter extends RecyclerView.Adapter<FlashcardAdapter.Flas
     public static class FlashcardViewHolder extends RecyclerView.ViewHolder {
         TextView wordTextView;
         TextView meaningTextView;
+        ImageView iconRemove;
 
         public FlashcardViewHolder(@NonNull View itemView) {
             super(itemView);
             wordTextView = itemView.findViewById(R.id.textViewWord);
             meaningTextView = itemView.findViewById(R.id.textViewMeaning);
+            iconRemove = itemView.findViewById(R.id.iconRemove);
         }
+    }
+    private void showRemoveDialog(int position) {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+        bottomSheetDialog.setContentView(R.layout.dialog_remove_flash); // Tạo file XML cho dialog
+
+        Button btnCancel = bottomSheetDialog.findViewById(R.id.btnCancel);
+        Button btnRemove = bottomSheetDialog.findViewById(R.id.btnRemove);
+
+        btnCancel.setOnClickListener(v -> bottomSheetDialog.dismiss());
+
+        btnRemove.setOnClickListener(v -> {
+            // Xóa flashcard khỏi màn hình
+//            LinearLayout layoutFlashcards = findViewById(R.id.flashContainer);
+//            layoutFlashcards.removeView(flashcardView);
+//            bottomSheetDialog.dismiss(); // Đóng hộp thoại sau khi xóa
+            flashcards.remove(position);  // Xóa khỏi danh sách
+            notifyItemRemoved(position); // Cập nhật RecyclerView
+            bottomSheetDialog.dismiss(); // Đóng dialog
+        });
+
+        bottomSheetDialog.show();
     }
 }
