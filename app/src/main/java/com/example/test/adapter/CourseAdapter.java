ed4023575f585da0166666147b7e55c59b2b108d
Trang <merinotran22@gmail.com>
2025-04-08 17:53:04 +0700

    sua margin intro course

--- app/src/main/java/com/example/test/adapter/CourseAdapter.java
+++ app/src/main/java/com/example/test/adapter/CourseAdapter.java
@@ -98,9 +98,9 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
                 textView.setTypeface(null, Typeface.BOLD); // Chữ in đậm
                 textView.setGravity(Gravity.CENTER);
 
-                int size = 90;
+                int size = 100;
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(size, size);
-                params.setMargins(10, 0, 10, 0); // Điều chỉnh khoảng cách giữa các lesson
+                params.setMargins(15, 0, 15, 0); // Điều chỉnh khoảng cách giữa các lesson
                 textView.setLayoutParams(params);
 
                 // Lấy ID bài học trước đó (nếu có)
